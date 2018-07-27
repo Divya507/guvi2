aee72aa24853aa6d9a6a3729fb8b99affc148021
Divya507 <41730737+Divya507@users.noreply.github.com>
2018-07-27 11:50:28 +0530

    Create eg4.java

---
+++ eg4.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int k,n;
+		int sum=0;
+		Scanner Sc=new Scanner(System.in);
+		n=Sc.nextInt();
+		k=Sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<k;i++)
+		{
+			a[i]=Sc.nextInt();
+		}
+		for(int i=0;i<k;i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+	}
+}
